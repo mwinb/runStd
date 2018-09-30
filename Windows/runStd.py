1deddb10b6c93f40ebbdf7372e11f22c7b095af8
mwinb <mwinberry0101@gmail.com>
2018-09-29 18:43:24 -0700

    Now Supports piping. compiled for linux

--- Windows/runStd.py
+++ Windows/runStd.py
@@ -7,9 +7,23 @@ def main():
     if len(sys.argv)-1 >= 1:
         try:
             toRun = sys.argv[1]
+            raw_input("4: " + something)
+            print("\n")
+            additionalArgs = "";
+            if len(sys.argv) > 2:
+                for i in range(2, len(sys.argv)):
+                    additionalArgs += " " + sys.argv[i]
+                    raw_input("1: " + additionalArgs + "\n")
+                    
             run("javac -Xlint:unchecked -cp .;stdlib.jar " + toRun)
+            
             toRun = toRun.replace(".java", "")
-            run("java -cp .;stdlib.jar " + toRun)
+            toRun = "java -cp .;stdlib.jar " + toRun
+            toRun += additionalArgs
+            raw_input("2: " + toRun + "\n")
+            
+            
+            run(toRun)
                 
         except Exception:
             print "----Invalid Command----"
