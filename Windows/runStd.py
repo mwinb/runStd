1b88f03b67c2e430721e6a9123315203dea16d1e
mwinb <mwinberry0101@gmail.com>
2018-09-29 19:40:58 -0700

    Compiled for Windows

--- Windows/runStd.py
+++ Windows/runStd.py
@@ -1,3 +1,4 @@
+
 import os
 import sys
 import platform
@@ -7,23 +8,12 @@ def main():
     if len(sys.argv)-1 >= 1:
         try:
             toRun = sys.argv[1]
-            raw_input("4: " + something)
-            print("\n")
-            additionalArgs = "";
+            run("javac -Xlint:unchecked -cp .;stdlib.jar " + toRun)
+            toRun = toRun.replace(".java", "")
             if len(sys.argv) > 2:
                 for i in range(2, len(sys.argv)):
-                    additionalArgs += " " + sys.argv[i]
-                    raw_input("1: " + additionalArgs + "\n")
-                    
-            run("javac -Xlint:unchecked -cp .;stdlib.jar " + toRun)
-            
-            toRun = toRun.replace(".java", "")
-            toRun = "java -cp .;stdlib.jar " + toRun
-            toRun += additionalArgs
-            raw_input("2: " + toRun + "\n")
-            
-            
-            run(toRun)
+                    toRun = toRun + " " + sys.argv[i]
+            run("java -cp .;stdlib.jar " + toRun)
                 
         except Exception:
             print "----Invalid Command----"
